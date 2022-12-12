a79b43592b710130d1aecd8c61b03a5696b194c1
Avitallidor <avitallidor8@hotmail.com>
2022-12-12 16:14:00 +0200

    Refactoring Actor Controller

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("./db");
 const cors = require("cors");
 
 const userRouter = require("./routes/user");
+const actorRouter = require("./routes/actor");
 const { handleNotFound } = require("./utils/helper");
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/user", userRouter);
+app.use("/api/actor", actorRouter);
 
 app.use("/*", handleNotFound);
 
