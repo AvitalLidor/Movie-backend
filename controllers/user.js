d7c28d7f16d8c4b09088fe12a4d5a2f99f5b3714
Avitallidor <avitallidor8@hotmail.com>
2022-12-21 20:28:03 +0200

    ..

--- controllers/user.js
+++ controllers/user.js
@@ -87,6 +87,7 @@ exports.verifyEmail = async (req, res) => {
       email: user.email,
       token: jwtToken,
       isVerified: user.isVerified,
+      role: user.role,
     },
     message: "Your email is verified.",
   });
@@ -226,11 +227,11 @@ exports.signIn = async (req, res, next) => {
   const matched = await user.comparePassword(password);
   if (!matched) return sendError(res, "Email/Password mismatch!");
 
-  const { _id, name, role, isVerified } = user;
+  const { _id, name, isVerified, role } = user;
 
   const jwtToken = jwt.sign({ userId: _id }, process.env.JWT_SECRET);
 
   res.json({
-    user: { id: _id, name, email, role, token: jwtToken, isVerified },
+    user: { id: _id, name, email, role, token: jwtToken, isVerified, role },
   });
 };
