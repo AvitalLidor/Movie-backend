7024ad5467603b74b9292d15dae610be496861ff
Avitallidor <avitallidor8@hotmail.com>
2023-01-31 12:27:14 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path"); //for live emails.
+// const path = require("path"); //for live emails.
 const morgan = require("morgan");
 const { errorHandler } = require("./middlewares/error");
 require("express-async-errors");
@@ -17,7 +17,7 @@ const { handleNotFound } = require("./utils/helper");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public"))); // for live emails.
+// app.use(express.static(path.join(__dirname, "public"))); // for live emails.
 app.use(morgan("dev"));
 app.use("/api/user", userRouter);
 app.use("/api/actor", actorRouter);
@@ -28,21 +28,13 @@ app.use("/api/admin", adminRouter);
 app.use("/*", handleNotFound);
 
 app.use(errorHandler);
-// app.post(
-//   "/sign-in",
-//   (req, res, next) => {
-//     const { email, password } = req.body;
-//     if (!email || !password)
-//       return res.json({ error: "email/password missing!" });
-//     next();
-//   },
-//   (req, res) => {
-//     res.send("<h1>hello im am from your backend about</h1>");
-//   }
-// );
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;  //live emails
 
-app.listen(PORT, () => {
-  console.log("the port is listeing on port " + PORT);
+// app.listen(PORT, () => {
+//   console.log("the port is listeing on port " + PORT);
+// });
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
 });
